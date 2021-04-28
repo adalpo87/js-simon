92ddd6e99380394c9e3ffd782c449d892e55aa86
adalpo87 <adalpo87@gmail.com>
2021-04-28 20:32:08 +0200

    terminato creazione array numeri random

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,32 +8,57 @@ simon-dice
  */
 
 $(function(){
-
+//creo tre array dove vengono salvati i numeri
   var arrayRandom = [];
   var myNumber = [];
   var numeriCorretti = [];
-//creo un array dove vengono salvati i numeri
+  var limite = 5;
+ 
+  reset();
+//clicco su via e mi compaiono i 5 numeri generati dal computer che vengono pushati nell'array
+  $('#btn-start').click(function () {
+    while(arrayRandom.length < limite){
+      var numero = numberGenerator(1, 99);
+      if(!arrayRandom.includes(numero)) arrayRandom.push(numero);
+    }
+  printDisplay('I numeri estratti sono ' + arrayRandom.join(' - '));
+  $('#btn-start').hide();
 
+  setTimeout(function () {
+    printDisplay("Indovina i 5 numeri");
+    $("#btn-box").show();
+  }, 5000);
 
-//clicco su via e mi compaiono i 5 numeri generati dal computer
-//devo creare una funzione di reset
+  })
+//dopo 5 secondi i numeri spariscono e compare l imput dove l utente inserisci i numeri
 
+//i numeri inseriti dall'utente verranno salvati in un array
 
+//i due array vengono confrontati, i numeri corrispondenti vengono pushati nel terzo array
 
 })
-
 
 
 
 //FUNZIONI
 //Funzione di reset
 function reset() {
+  arrayRandom =[];
+  myNumber =[];
+  numeriCorretti =[];
+  printDisplay('Per giocare premi VIA')
   $('#btn-box').hide();
   $('#btn-start').show();
-  $('#btn-restart').show();
+  $('#btn-restart').hide();
+  $('#display').show();
 }
 
-//Creo una funzione che mi genera numeri
+//funzione che genera testo
 function printDisplay(text) {
   $('#display').text(text);
+}
+
+//funzione che genera numeri
+function numberGenerator(min, max){
+  return Math.floor(Math.random()*(max - min + 1)+min);
 }
